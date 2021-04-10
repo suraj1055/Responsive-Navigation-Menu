d869bc69e8e3de7f85671b2c3883325a43d52a12
Suraj vishwakarma <vishwakarmasuraj631@gmail.com>
2021-04-10 11:49:52 +0530

    COMPLETED RESPONSIVE NAVBAR

---
+++ js/app.js
@@ -0,0 +1,39 @@
+window.addEventListener('resize', function() {
+    addRequiredClass();
+})
+
+function addRequiredClass() {
+    if(window.innerWidth < 750) {
+        document.body.classList.add('mobile')
+    }
+    else {
+        document.body.classList.remove('mobile')
+    }
+}
+
+window.onload = addRequiredClass
+
+
+let Hamburger = document.querySelector('.Hamburger')
+
+let MobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.Hamburger span')
+
+let isActive = false
+
+Hamburger.addEventListener('click', function() {
+    MobileNav.classList.toggle('open')
+    if (!isActive) {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    }
+    else{
+        bars[0].style.transform = 'rotate(0deg)'
+        bars[1].style.opacity = '1'
+        bars[2].style.transform = 'rotate(0deg)'
+        isActive = false
+    }
+})
